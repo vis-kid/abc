0d595a1a85f333231867dae2c9611b8b68422542
ed wassermann <visavis.kid@gmail.com>
2012-09-07 17:43:57 +0200

    finish signup form

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+ FactoryGirl.define do
+   factory :user do
+     name     "Edgar Hoover"
+     email    "edgar@example.com"
+     password "foobar"
+     password_confirmation "foobar"
+   end	
+ end
